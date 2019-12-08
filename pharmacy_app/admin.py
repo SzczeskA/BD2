ff000f7af3df7f475689dedaa472f9ba141b2b0c
SzczeskA <241293@student.pwr.edu.pl>
2019-12-08 14:01:08 +0100

    container added

--- pharmacy_app/admin.py
+++ pharmacy_app/admin.py
@@ -3,7 +3,15 @@ from django.contrib import admin
 from .models import Apteka
 from .models import Pracownik
 from .models import Pracownik_Apteka
+from .models import Sub_czynna
+from .models import Lek
+from .models import Sub_czynna_Lek
+from .models import Opakowanie
 
 admin.site.register(Apteka)
 admin.site.register(Pracownik)
 admin.site.register(Pracownik_Apteka)
+admin.site.register(Sub_czynna)
+admin.site.register(Lek)
+admin.site.register(Sub_czynna_Lek)
+admin.site.register(Opakowanie)
--- pharmacy_app/models.py
+++ pharmacy_app/models.py
@@ -3,15 +3,31 @@ from django.db import models
 from django.utils import timezone
 
 class Apteka (models.Model):
-    Nazwa = models.CharField(max_length=30)
-    Adres = models.CharField(max_length=30)
-    Kod_pocztowy = models.CharField(max_length=6)
+    Nazwa = models.CharField(max_length=30, null=False)
+    Adres = models.CharField(max_length=30, null=False)
+    Kod_pocztowy = models.CharField(max_length=6, null=False)
 
 class Pracownik (models.Model):
-    HASH_hasla = models.CharField(max_length=30)
-    LOGIN = models.CharField(max_length=30, unique= True)
-    Poziom_dostpu = models.IntegerField(max_length=2)
+    HASH_hasla = models.CharField(max_length=30, null=False)
+    LOGIN = models.CharField(max_length=30, unique= True, null=False)
+    Poziom_dostpu = models.IntegerField(max_length=2, null=False)
 
 class Pracownik_Apteka (models.Model):
     Pracownik_ID = models.ForeignKey(Pracownik, on_delete=models.CASCADE)
     Apteka_ID = models.ForeignKey(Apteka, on_delete=models.CASCADE)
+
+class Sub_czynna (models.Model):
+    Nazwa = models.CharField(max_length=30, null=False)
+
+class Lek (models.Model):
+    Nazwa = models.CharField(max_length=30, null=False)
+    Kraj_poch = models.CharField(max_length=30, null=False)
+
+class Sub_czynna_Lek (models.Model):
+    Lek_ID = models.ForeignKey(Lek, on_delete=models.CASCADE)
+    Sub_ID = models.ForeignKey(Sub_czynna, on_delete=models.CASCADE)
+
+class Opakowanie (models.Model):
+    Ile_dawek= models.IntegerField(max_length= 10, null=False)
+    Jednostka_dawki= models.IntegerField(max_length= 10, null=False)
+    Lek_ID = models.ForeignKey(Lek, on_delete=models.CASCADE)
