cc24845eea1b8d335e59e1246403e767cd960094
wamrugala <wamrugala@gmail.com>
2019-12-18 17:25:18 +0100

    Restored clients/views

--- clients/views.py
+++ clients/views.py
@@ -4,24 +4,6 @@ from django.http import HttpResponse
 # from django.core.shortcuts import render, redirect
 from django import forms
 from django.utils import timezone
-from clients.forms import KlientForm
-
-
-def add_model(request):
-
-    if request.method == "POST":
-        form = KlientForm(request.POST)
-        if form.is_valid():
-            model_instance = form.save(commit=False)
-            model_instance.timestamp = timezone.now()
-            model_instance.save()
-            return redirect('/')
-
-    else:
-
-        form = KlientForm()
-
-        return render(request, "my_template.html", {'form': form})
 
 
 def index(request):
