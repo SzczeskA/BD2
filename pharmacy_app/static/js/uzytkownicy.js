a7ed1194ebb6c44a4da7c9da0620196ea62cffad
wamrugala <wamrugala@gmail.com>
2020-01-27 07:00:42 +0100

    Deleting of users fixed

--- pharmacy_app/static/js/uzytkownicy.js
+++ pharmacy_app/static/js/uzytkownicy.js
@@ -50,7 +50,7 @@ $(document).ready(function(){
                 console.log(event.currentTarget.id);
                 $.ajax({
                         method: "POST",
-                        url: "/aptekarze-app/usun",
+                        url: "/uzytkownicy-app/usun",
                         dataType: "json",
                         contentType: "application/json; charset=utf-8",
                         data: JSON.stringify({
